10620b8197d011057eb1892b78f4bbeffb959230
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-07-27 13:35:41 +0200

    Update header.js

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -22,27 +22,32 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 */
 
+
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', () => {
+        // Toggle zwischen Dark- und Light-Mode
         if (document.body.classList.contains('dark-mode')) {
             document.body.classList.remove('dark-mode');
             document.body.classList.add('light-mode');
-            themeToggle.textContent = '🌙';
+            themeToggle.textContent = '🌙'; // Mond-Emoji für Light-Mode
         } else {
             document.body.classList.remove('light-mode');
             document.body.classList.add('dark-mode');
-            themeToggle.textContent = '☀️';
+            themeToggle.textContent = '☀️'; // Sonne-Emoji für Dark-Mode
         }
+        // Speichern der bevorzugten Theme-Einstellung im Local Storage
         localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
     });
 
+    // Beim Laden der Seite das gespeicherte Theme anwenden
     const savedTheme = localStorage.getItem('theme') || 'light';
     if (savedTheme === 'dark') {
         document.body.classList.add('dark-mode');
-        themeToggle.textContent = '☀️';
+        themeToggle.textContent = '☀️'; // Sonne-Emoji für Dark-Mode
     } else {
         document.body.classList.add('light-mode');
-        themeToggle.textContent = '🌙';
+        themeToggle.textContent = '🌙'; // Mond-Emoji für Light-Mode
     }
 });
+
